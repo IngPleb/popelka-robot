67b9f80b35e04d4fa6ec1b3c68c4944d34ef3df7
Adam Handl <fajn3x@gmail.com>
2024-11-12 18:01:09 +0100

    motory

--- main.py
+++ main.py
@@ -9,11 +9,20 @@ from pybricks.tools import wait
 
 ev3 = EV3Brick()
 
+
+celkem_vzdalensost = 8000 # celkem vzdálenost kterou to má ujet
+koeficient_vzdalenosti = 160 #bylo by to moc velký, ale mohla by to klidně být jedn
+korekce_vzdalenosti_dole = 7 #procenta navíc dole z vdálenosti
+rychlost_zvedani = 1000 
+rychlost_klesani = 650
+
+
 # Motors
-motor_A = Motor(Port.A)
+motor_B = Motor(Port.B)
 
-motor_A.reset_angle(0)
+motor_B.reset_angle(0)
 
-motor_A.run_time(1000, 2000)
+motor_B.run_time(rychlost_klesani, celkem_vzdalensost*(1+0.01*korekce_vzdalenosti_dole)*koeficient_vzdalenosti/rychlost_klesani) 
 
-motor_A.run_time(-400, 1000)
+
+motor_B.run_time(-rychlost_zvedani, celkem_vzdalensost*koeficient_vzdalenosti/rychlost_zvedani)
