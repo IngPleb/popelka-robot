465117506e920ec889bd39d2d40f416ed73191de
Matěj Vnouček <matej.vnoucek@icloud.com>
2024-11-18 23:40:15 +0100

    feat: Add theoretical routine

--- main.py
+++ main.py
@@ -1,48 +1,63 @@
 #!/usr/bin/env pybricks-micropython
 
-from pybricks.hubs import EV3Brick
 from pybricks.parameters import Port
 
 from devices.SimpleUltraSonic import SimpleUltraSonic
-
-from systems.LiftSystem import LiftSystem
 from systems.DriveSystem import DriveSystem
+from systems.LiftSystem import LiftSystem
 from systems.LightSystem import LightSystem
 
-# Configuration
-LEFT_MOTOR_PORT = Port.A
-RIGHT_MOTOR_PORT = Port.C
-COLOR_SENSOR_PORT = Port.S1
-LIFT_PORT = Port.D
+# Ports
+#######################
+# Sensors
+light_port = Port.S1
+lift_port = Port.D
+ultra_sonic_port = Port.S2
 
-# Initialize the EV3 Brick
-ev3 = EV3Brick()
+# Motors
+left_motor_port = Port.A
+right_motor_port = Port.C
 
-def initialize_lift_system():
-    lift_system = LiftSystem(LIFT_PORT)
-    return lift_system
 
-def initialize_drive_system():
-    drive_system = DriveSystem(LEFT_MOTOR_PORT, RIGHT_MOTOR_PORT, COLOR_SENSOR_PORT)
-    return drive_system
-
-# In main.py - test sequence:
 def main():
-    ev3.speaker.beep()
-    
-    drive_system = DriveSystem(left_motor_port=Port.A, right_motor_port=Port.C, wheel_diameter_mm=68.8,
+    # Initialize needed systems with Dependency Injection
+    #######################
+    light_system = LightSystem(light_port, blue_threshold_on_line=7)
+    ultra_sonic_sensor = SimpleUltraSonic(ultra_sonic_port, 120)
+    lift_system = LiftSystem(lift_port)
+    drive_system = DriveSystem(left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+                               wheel_diameter_mm=68.8,
                                axle_track_mm=92.5,
-                               base_speed=200, correction_factor=35, light_system=LightSystem,
-                               lift_system=LiftSystem(LIFT_PORT), simple_ultra_sonic=SimpleUltraSonic)
-    print("Starting forward movement with correction...")
-    drive_system.move_distance(1250,True)
-    
-    print("Rotating...")
-    drive_system.rotate(angle=90, speed=300)
-    
-    print("Program completed.")
+                               base_speed=200, correction_factor=35, light_system=light_system,
+                               lift_system=lift_system, simple_ultra_sonic=ultra_sonic_sensor)
 
-        
+    # Routine instructions
+    # Four perpendicular lines from the main line
+    for i in range(4):
+        # The distance is theoretically 840 mm
+        # We will see based on the Adam's empirical data  (750 with correction + 75 without correction)
+        drive_system.move_distance(840, True)
+        drive_system.rotate_angle(180)
+        # Returning back to the main line
+        drive_system.move_distance(800, True)
+        # We have noticed in earlier tests that the robot will make errors at crossing the perpendicular lines
+        drive_system.move_distance(40, False)
+
+        # Connecting to the main line
+        drive_system.rotate_angle(90)  # TODO: find out if it is + or -
+        drive_system.move_distance(280, False)
+        drive_system.rotate_angle(-90)
+
+    # Adam's empirical data (we will use it for the adjusting the theoretical routine)
+    # drive_system.move_distance(750, True)  # true for turn on the correction
+    # drive_system.move_distance(75, False)
+    # drive_system.move_distance(-25, False)
+    # drive_system.rotate_angle(-340)
+    # drive_system.move_distance(200, False)
+    # drive_system.move_distance(-50, False)
+    # drive_system.rotate_angle(-350)
+    # drive_system.move_distance(750, True)
+
 
 if __name__ == "__main__":
     main()
--- playground.py
+++ playground.py
@@ -1,30 +1,8 @@
 #!/usr/bin/env pybricks-micropython
 
-from pybricks.parameters import Port
-
-from devices.SimpleUltraSonic import SimpleUltraSonic
-from systems.DriveSystem import DriveSystem
-from systems.LiftSystem import LiftSystem
-from systems.LightSystem import LightSystem
-
-lift_port = Port.D
-
 
 def main():
-    light_system = LightSystem(Port.S1, blue_threshold_on_line=7)
-    ultra_sonic_sensor = SimpleUltraSonic(Port.S2, 120)
-    drive_system = DriveSystem(left_motor_port=Port.A, right_motor_port=Port.C, wheel_diameter_mm=68.8,
-                               axle_track_mm=92.5,
-                               base_speed=200, correction_factor=35, light_system=light_system,
-                               lift_system=LiftSystem(lift_port), simple_ultra_sonic=ultra_sonic_sensor)
-    drive_system.move_distance(750, True)  # true for turn on the correction
-    drive_system.move_distance(75, False)
-    drive_system.move_distance(-25, False)
-    drive_system.rotate_angle(-340)
-    drive_system.move_distance(200, False)
-    drive_system.move_distance(-50, False)
-    drive_system.rotate_angle(-350)
-    drive_system.move_distance(750, True)
+    print("Welcome to the playground.py\n Here you can test your code")
 
 
 if __name__ == "__main__":
